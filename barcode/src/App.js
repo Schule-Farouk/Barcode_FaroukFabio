0535e7c75975e2bfee4618ac3cf132e034969ed3
Farouk Saadeh <FaroukIzzDaa@hotmail.com>
2024-03-28 08:52:23 +0100

    Added Folders

--- barcode/src/App.js
+++ barcode/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Aufgabe1 from './Farouk';
+import Aufgabe3 from './Fabio';
+import Aufgabe4 from './Jony';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Startseite</h1> 
+      <h2>Spezialitäten</h2>
+      <div>
+        
+      <Farouk />
+      <Fabio />
+      <Jony />
+
+      </div>
     </div>
   );
 }
